b519ee2c5fd43fc13159b94eb8e3b27ef0703d88
drmrd <drmrd@users.noreply.github.com>
2016-09-10 18:39:46 -0400

    Enable summing I and I (passing 2nd test)

--- src/roman_calculator.c
+++ src/roman_calculator.c
@@ -2,5 +2,5 @@
 
 char *add_roman_numerals(const char *summand1, const char *summand2)
 {
-    return "";
+    return "II";
 }
--- tests/check_roman_calculator.c
+++ tests/check_roman_calculator.c
@@ -9,6 +9,11 @@ START_TEST(addition_function_accepts_two_strings)
     add_roman_numerals("I", "I");
 END_TEST
 
+START_TEST(sum_of_I_and_I_is_II)
+    char *sum = add_roman_numerals("I", "I");
+    ck_assert_str_eq(sum, "II");
+END_TEST
+
 Suite *create_calculator_test_suite(void)
 {
     Suite *test_suite = suite_create("Roman_Calculator");
@@ -16,6 +21,7 @@ Suite *create_calculator_test_suite(void)
     TCase *addition_test_case = tcase_create("Addition");
 
     tcase_add_test(addition_test_case, addition_function_accepts_two_strings);
+    tcase_add_test(addition_test_case, sum_of_I_and_I_is_II);
 
     suite_add_tcase(test_suite, addition_test_case);
 
